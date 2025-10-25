21756a8b5704875d034d04ed76843eb40d3b7219
Vinoth Kannan <svkn.87@gmail.com>
2025-10-25 12:42:20 +0530

    dev: call setHeading inside useEffect method

--- components/breadcrumb-heading.tsx
+++ components/breadcrumb-heading.tsx
@@ -1,9 +1,14 @@
 "use client"
 
 import { useBreadcrumb } from "@/context/BreadcrumbContext"
+import { useEffect } from "react"
 
 export default function BreadcrumbHeading({ text }: { text: string }) {
   const { setHeading } = useBreadcrumb()
-  setHeading(text)
+
+  useEffect(() => {
+    setHeading(text)
+  }, [text, setHeading])
+
   return null
 }
