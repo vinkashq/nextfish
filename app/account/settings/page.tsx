f2e34ce96edc437e66872f35249af52e7946b463
Vinoth Kannan <svkn.87@gmail.com>
2025-10-27 14:17:44 +0530

    ux: move item actions to footer to improve readability in mobile

--- app/account/settings/page.tsx
+++ app/account/settings/page.tsx
@@ -3,7 +3,7 @@
 import BreadcrumbHeading from "@/components/breadcrumb-heading"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle } from "@/components/ui/item"
+import { Item, ItemActions, ItemContent, ItemDescription, ItemFooter, ItemGroup, ItemHeader, ItemTitle } from "@/components/ui/item"
 import { Spinner } from "@/components/ui/spinner"
 import { useFirebase } from "@/firebase/client"
 import { useCurrentUser } from "@/firebase/client/auth"
@@ -49,23 +49,25 @@ export default function Settings() {
       <h1 className="scroll-m-20 text-left text-4xl font-extrabold tracking-tight text-balance">
         Account Settings
       </h1>
-
+      <ItemGroup className="gap-4">
       <Item variant="outline">
         <ItemContent>
           <ItemTitle>Email Address</ItemTitle>
           <ItemDescription>We use your email address to send you important notifications and security updates.</ItemDescription>
         </ItemContent>
-        <ItemActions>
-          {user.email ? (
-            <Input type="email" 
-                   value={user.email} 
-                   readOnly
-                   disabled
-                   placeholder="Email address" />
-          ) : (
-            <div>No email address set</div>
-          )}
-        </ItemActions>
+        <ItemFooter>
+          <ItemActions>
+            {user.email ? (
+              <Input type="email" 
+                    value={user.email} 
+                    readOnly
+                    disabled
+                    placeholder="Email address" />
+            ) : (
+              <div>No email address set</div>
+            )}
+          </ItemActions>
+        </ItemFooter>
       </Item>
 
       <Item variant="outline">
@@ -75,13 +77,16 @@ export default function Settings() {
             We will send you an email to change your password.
           </ItemDescription>
         </ItemContent>
-        <ItemActions>
-          <Button className="font-normal" variant="outline" size="sm" onClick={resetPassword} disabled={sendingPasswordResetEmail}>
-            <Spinner variant="visibleOnDisabled" aria-disabled={sendingPasswordResetEmail} />
-            Send Password Reset Email
-          </Button>
-        </ItemActions>
+        <ItemFooter>
+          <ItemActions>
+            <Button className="font-normal" variant="outline" size="sm" onClick={resetPassword} disabled={sendingPasswordResetEmail}>
+              <Spinner variant="visibleOnDisabled" aria-disabled={sendingPasswordResetEmail} />
+              Send Password Reset Email
+            </Button>
+          </ItemActions>
+        </ItemFooter>
       </Item>
+      </ItemGroup>
     </div>
   )
 }
