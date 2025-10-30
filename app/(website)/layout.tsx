dda904a234e6e0e63b2a158e4cd5b2693e708159
Vinoth Kannan <svkn.87@gmail.com>
2025-10-30 13:10:36 +0530

    ux: add padding to main section of the website layout

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -9,7 +9,7 @@ export default function WebsiteLayout({
   return (
     <div className="container mx-auto flex flex-col justify-between px-2 md:px-4 lg:px-6 min-h-svh">
       <Header />
-      <main className="my-4 flex flex-col items-center justify-center grow">
+      <main className="my-4 flex flex-col items-center justify-center px-2 md:px-4 lg:px-6 grow">
         {children}
       </main>
       <Footer />
