c4bd0483a54ab9f397ce586ce7afcc1fc2b44e6a
Vinoth Kannan <svkn.87@gmail.com>
2025-11-03 12:36:55 +0530

    dev: minor changes in code formatting

--- config/menu/admin.ts
+++ config/menu/admin.ts
@@ -4,15 +4,19 @@ import { LayoutDashboard, Users } from "lucide-react"
 export const defaultMenu: Menu = {
   title: "Admin Console",
   baseUrl: "/admin",
-  items: [{
-    title: "Dashboard",
-    url: "/",
-    icon: LayoutDashboard,
-  }, {
-    title: "Users",
-    url: "/users/",
-    icon: Users,
-  }]
+  items: [
+    {
+      title: "Dashboard",
+      url: "/",
+      icon: LayoutDashboard,
+    }, {
+      title: "Users",
+      url: "/users/",
+      icon: Users,
+    },
+  ]
 }
 
-export const menus: Menu[] = [defaultMenu]
+export const menus: Menu[] = [
+  defaultMenu,
+]
