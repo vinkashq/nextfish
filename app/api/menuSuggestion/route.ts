4a938d026a6f2801a17703f84eb3b9cce11ef398
Vinoth Kannan <svkn.87@gmail.com>
2025-11-03 19:14:01 +0530

    add sample menu suggestion api

---
+++ app/api/menuSuggestion/route.ts
@@ -0,0 +1,4 @@
+import { menuSuggestionFlow } from '@/firebase/server/genkit'
+import { appRoute } from '@genkit-ai/next'
+
+export const POST = appRoute(menuSuggestionFlow)
--- firebase/server/genkit.ts
+++ firebase/server/genkit.ts
@@ -3,13 +3,38 @@
 const googleGenAIApiKey = process.env.GOOGLE_GENAI_API_KEY
 
 import { gemini20Flash, googleAI } from '@genkit-ai/googleai';
+import { genkit, z } from 'genkit';
 
 const genkitConfig = {
   plugins: [googleAI()],
   model: gemini20Flash,
 }
 
+const ai = genkit(genkitConfig)
+
+const menuSuggestionFlow = ai.defineFlow(
+  {
+    name: 'menuSuggestionFlow',
+    inputSchema: z.object({ theme: z.string() }),
+    outputSchema: z.object({ menuItem: z.string() }),
+    streamSchema: z.string(),
+  },
+  async ({ theme }, { sendChunk }) => {
+    const { stream, response } = ai.generateStream({
+      model: googleAI.model('gemini-2.5-flash'),
+      prompt: `Invent a menu item for a ${theme} themed restaurant.`,
+    });
+
+    for await (const chunk of stream) {
+      sendChunk(chunk.text);
+    }
+
+    const { text } = await response;
+    return { menuItem: text };
+  }
+);
+
+
 export {
-  googleGenAIApiKey,
-  genkitConfig
+  menuSuggestionFlow
 }
