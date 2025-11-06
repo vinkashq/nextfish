a794108e196d5f5f11580854e200cdefa9dbc2f3
Vinoth Kannan <svkn.87@gmail.com>
2025-11-06 18:12:19 +0530

    dev: use SERVICE_ACCOUNT_KEY secret to use to initialize app in GitHub actions

--- src/firebase/server/index.ts
+++ src/firebase/server/index.ts
@@ -1,9 +1,26 @@
-import { initializeApp } from "firebase-admin/app"
+"use server"
+
+import { App, applicationDefault, getApp, initializeApp, ServiceAccount } from "firebase-admin/app"
 import { getFirestore } from "firebase-admin/firestore"
 import { getStorage } from "firebase-admin/storage"
 import { getAuth } from "firebase-admin/auth"
 
-const app = initializeApp()
+let app: App
+
+try {
+  app = getApp()
+} catch {
+  if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    app = initializeApp({
+      credential: applicationDefault()
+    })
+  } else if (process.env.SERVICE_ACCOUNT_KEY) {
+    app = initializeApp(process.env.SERVICE_ACCOUNT_KEY as ServiceAccount)
+  } else {
+    app = initializeApp()
+  }
+}
+
 const auth = getAuth(app)
 const firestore = getFirestore(app)
 const storage = getStorage(app)
