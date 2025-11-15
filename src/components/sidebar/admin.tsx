ed2c65815cb9928fc4af9817d70f388d152ed706
Vinoth Kannan <svkn.87@gmail.com>
2025-11-15 15:52:39 +0530

    refactor: simplify AdminSidebar component structure
    - Removed the Accordion component and directly rendered SidebarGroup elements for each menu item.
    - Improved layout by adding margin to SidebarContent for better spacing.
    - Enhanced readability by streamlining the conditional rendering logic for single and multiple menu items.

--- src/components/sidebar/admin.tsx
+++ src/components/sidebar/admin.tsx
@@ -9,36 +9,27 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar"
 import { menus } from "@/config/menu/admin"
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion"
 import { ComponentProps } from "react"
 
 export default function AdminSidebar({ ...props }: ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar className="sticky top-(--header-height) h-[calc(100svh-var(--header-height))]!" collapsible="icon" {...props}>
-      <SidebarContent>
-        <Accordion type="single" collapsible defaultValue="0">
-          {menus.map((menu, index) => (
-            <AccordionItem key={index} value={index.toString()}>
-              <SidebarGroup>
-                {menus.length === 1 && (
-                  <SidebarNav baseUrl={menu.baseUrl} items={menu.items} />
-                )}
-                {menus.length > 1 && (
-                  <>
-                    <AccordionTrigger className="py-1">
-                      <SidebarGroupLabel className="h-4">
-                        {menu.title}
-                      </SidebarGroupLabel>
-                    </AccordionTrigger>
-                    <AccordionContent asChild>
-                      <SidebarNav baseUrl={menu.baseUrl} items={menu.items} />
-                    </AccordionContent>
-                  </>
-                )}
-              </SidebarGroup>
-            </AccordionItem>
-          ))}
-        </Accordion>
+      <SidebarContent className="mt-2">
+        {menus.map((menu, index) => (
+          <SidebarGroup key={index}>
+            {menus.length === 1 && (
+              <SidebarNav baseUrl={menu.baseUrl} items={menu.items} />
+            )}
+            {menus.length > 1 && (
+              <>
+                <SidebarGroupLabel className="h-4">
+                  {menu.title}
+                </SidebarGroupLabel>
+                <SidebarNav baseUrl={menu.baseUrl} items={menu.items} />
+              </>
+            )}
+          </SidebarGroup>
+        ))}
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
