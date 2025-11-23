6f6204b02b88c7ab81a8e138d8ee37fd61eee9ba
Vinoth Kannan <svkn.87@gmail.com>
2025-11-23 19:21:46 +0530

    ux: allow custom class names to be passed to the UserAvatar component

--- src/components/user/avatar.tsx
+++ src/components/user/avatar.tsx
@@ -1,5 +1,6 @@
 import { AvatarImage } from "@radix-ui/react-avatar";
 import { Avatar, AvatarFallback } from "../ui/avatar";
+import { cn } from "@/lib/utils";
 
 type UserAvatarProps = {
   displayName?: string | null;
@@ -9,14 +10,16 @@ type UserAvatarProps = {
 
 export default function UserAvatar({
   user,
+  className,
 }: {
   user: UserAvatarProps | null | undefined
+  className?: string
 }) {
   if (!user) return null
 
   const initials = (user.displayName || user.email || "U").slice(0, 2).toUpperCase()
   return (
-    <Avatar className="h-8 w-8 rounded-full">
+    <Avatar className={cn("h-8 w-8 rounded-full", className)}>
       <AvatarImage src={user.photoURL || undefined} alt={user.displayName || user.email || undefined} />
       <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
     </Avatar>
